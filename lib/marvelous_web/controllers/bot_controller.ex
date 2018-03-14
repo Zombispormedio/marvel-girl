45d784b5c8d83c45f057c8263d66983c4a64ed6e
Xavier Serrano <zombispormedio007@gmail.com>
2018-03-14 20:57:50 +0100

    see params

--- lib/marvelous_web/controllers/bot_controller.ex
+++ lib/marvelous_web/controllers/bot_controller.ex
@@ -1,7 +1,8 @@
 defmodule MarvelousWeb.BotController do
     use MarvelousWeb, :controller
   
-    def execute(conn, _params) do
+    def execute(conn, params) do
+        IO.inspect params
         response = "This is a sample response from your webhook!"
         render(conn, "response.json", response: response)
     end
