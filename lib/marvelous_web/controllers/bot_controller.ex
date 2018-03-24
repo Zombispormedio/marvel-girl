1060f44bd53a3856d594fa841a0983deb490d7f4
Xavier Serrano <zombispormedio007@gmail.com>
2018-03-24 19:16:43 +0100

    fix bot

--- lib/marvelous_web/controllers/bot_controller.ex
+++ lib/marvelous_web/controllers/bot_controller.ex
@@ -2,7 +2,7 @@ defmodule MarvelousWeb.BotController do
     use MarvelousWeb, :controller
   
     def execute(conn, %{ "result" => result }) do
-        IO.puts result
+        IO.inspect result
         response = "I am in it"
         render(conn, "response.json", response: response)
     end
