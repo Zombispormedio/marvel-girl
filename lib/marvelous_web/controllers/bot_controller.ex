766a589c4dcd7ded3508e6863abd23430455aba1
Xavier Serrano <zombispormedio007@gmail.com>
2018-03-24 19:10:14 +0100

    add supoort to all

--- lib/marvelous_web/controllers/bot_controller.ex
+++ lib/marvelous_web/controllers/bot_controller.ex
@@ -1,8 +1,9 @@
 defmodule MarvelousWeb.BotController do
     use MarvelousWeb, :controller
   
-    def execute(conn, %{"result" => %{"parameters" => %{"geo-city" => city}}}) do
-        response = "The Weather in #{city} is good"
+    def execute(conn, %{ result }) do
+        IO.puts result
+        response = "I am in it"
         render(conn, "response.json", response: response)
     end
   end
