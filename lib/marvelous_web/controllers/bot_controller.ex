6c95ec4714b0937cacba10dd4c47e97cbf310bb4
Xavier Serrano <zombispormedio007@gmail.com>
2018-03-24 19:25:10 +0100

    add support to last issue

--- lib/marvelous_web/controllers/bot_controller.ex
+++ lib/marvelous_web/controllers/bot_controller.ex
@@ -3,8 +3,17 @@ defmodule MarvelousWeb.BotController do
   
     def execute(conn, %{ "result" => result }) do
         IO.inspect result
-        response = "I am in it"
+        response = makeResponse(result)
         render(conn, "response.json", response: response)
+    end
+
+    defp makeResponse(%{ "action" => "get_last_issue", "parameters" => parameters }) do
+        IO.inspect parameters
+        "I'm getting the last issue"
+    end
+
+    defp makeResponse(_result) do
+        "I'm not sure about that"
     end
   end
   
